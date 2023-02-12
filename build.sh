3615f24da111f9946a18c082bfa5a3b9484ceb5e
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-12 19:28:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,6 @@
 
 printf "\e[1;32m \u2730 Recovery Compiler\e[0m\n\n"
 
-# Source Configs
-source $CONFIG
-
-# Echo Loop
-while ((${SECONDS_LEFT:=10} > 0)); do
-    printf "Please wait %.fs ...\n" "${SECONDS_LEFT}"
-    sleep 1
-    SECONDS_LEFT=$((SECONDS_LEFT - 1))
-done
-unset SECONDS_LEFT
-
 echo "::group::Mandatory Variables Checkup"
 if [[ -z ${MANIFEST} ]]; then
     printf "Please Provide A Manifest URL with/without Branch\n"
@@ -31,15 +20,14 @@ echo "::endgroup::"
 
 printf "We are going to build ${FLAVOR}-flavored ${TARGET} for ${CODENAME} from the manufacturer ${VENDOR}\n"
 
-# cd To An Absolute Path
-mkdir -p ~/runner/builder &>/dev/null
-cd ~ || exit 1
-
+cd ~
+mkdir pbrp
+cd pbrp
 echo "::group::Source Repo Sync"
 printf "Initializing Repo\n"
 printf "We will be using %s for Manifest source\n" "${MANIFEST}"
-repo init -q -u ${MANIFEST} --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips || { printf "Repo Initialization Failed.\n"; exit 1; }
-repo sync -c -q --force-sync --no-clone-bundle --no-tags -j6 || { printf "Git-Repo Sync Failed.\n"; exit 1; }
+repo init -u ${MANIFEST} || { printf "Repo Initialization Failed.\n"; exit 1; }
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags || { printf "Git-Repo Sync Failed.\n"; exit 1; }
 echo "::endgroup::"
 
 echo "::group::Device and Kernel Tree Cloning"
@@ -52,26 +40,6 @@ if [[ ! -z "${KERNEL_LINK}" ]]; then
     git clone ${KERNEL_LINK} --depth=1 kernel/${VENDOR}/${CODENAME}
 else
     printf "Using Prebuilt Kernel For The Build.\n"
-fi
-echo "::endgroup::"
-
-echo "::group::Secret Bootable"
-if [[ $USE_SECRET_BOOTABLE == 'true' ]] && [[ -z "$SECRET_BR" ]]; then
-    printf "Secret Branch is Not Defined\n"
-elif [[ $USE_SECRET_BOOTABLE == 'true' ]] && [[ ! -z "$SECRET_BR" ]]; then
-    rm -rf bootable/recovery
-    printf "Cloning Secret Bootable\n"
-    git clone --quiet --progress https://pbrp-bot:$GH_BOT_TOKEN@github.com/PitchBlackRecoveryProject/pbrp_recovery_secrets -b ${SECRET_BR} --single-branch bootable/recovery
-else
-    printf "Using Default Bootable\n"
-fi
-echo "::endgroup::"
-
-echo "::group::Extra Commands"
-if [[ ! -z "$EXTRA_CMD" ]]; then
-    printf "Executing Extra Commands\n"
-    eval "${EXTRA_CMD}" || { printf "Failed While Executing Extra Commands.\n"; exit 1; }
-    cd /home/runner/builder || exit
 fi
 echo "::endgroup::"
 
@@ -93,9 +61,6 @@ echo "::group::Compilation"
 mka -j 2 ${TARGET} || { printf "Compilation failed.\n "; free -h; exit 1; }
 echo "::endgroup::"
 
-# Export VENDOR, CODENAME and BuildPath for next steps
-echo "VENDOR=${VENDOR}" >> ${CIRRUS_ENV}
-echo "CODENAME=${CODENAME}" >> ${CIRRUS_ENV}
-echo "BuildPath=~/runner/builder" >> ${CIRRUS_ENV}
-
-# TODO:: Add GitHub Release Script Here
+sudo apt-get update && sudo apt-get install sshpass -y
+cd ~/pbrp
+bash vendor/utils/pb_deploy.sh ${BUILD_RELEASE_TYPE} ${VENDOR} ${CODENAME}
